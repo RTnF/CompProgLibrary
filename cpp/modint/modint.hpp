3993462ab92078201efbb814da8a55f9fdeb084d
RTnF <50510242+RTnF@users.noreply.github.com>
2024-10-31 21:49:33 +0900

    modint mod

--- cpp/modint/modint.hpp
+++ cpp/modint/modint.hpp
@@ -6,7 +6,7 @@
  * based on ACL
  * @tparam mod 素数に限る
  */
-template <uint32_t mod, enable_if_t<(2 <= mod)> * = nullptr> class ModInt {
+template <uint32_t m, enable_if_t<(2 <= m)> * = nullptr> class ModInt {
   uint32_t x;
 
 public:
@@ -17,6 +17,7 @@ public:
   constexpr ModInt(U x_) : x(x_ % mod) {}
 
   uint32_t get() const { return x; }
+  static constexpr uint32_t mod() { return m; }
   template <class T, enable_if_t<is_signed_v<T>> * = nullptr>
   constexpr void set(T x_) {
     x = x_ < 0 ? x_ % (ll)mod + (ll)mod : x_ % (ll)mod;
@@ -32,7 +33,7 @@ public:
     return y;
   }
   constexpr ModInt &operator++() {
-    x = x == mod - 1 ? 0u : x + 1;
+    x = x == m - 1 ? 0u : x + 1;
     return *this;
   }
   constexpr ModInt operator--(int) {
@@ -41,30 +42,30 @@ public:
     return y;
   }
   constexpr ModInt &operator--() {
-    x = x == 0u ? mod - 1 : x - 1;
+    x = x == 0u ? m - 1 : x - 1;
     return *this;
   }
   constexpr ModInt &operator+=(const ModInt &y) {
     x += y.x;
-    if (x >= mod) {
-      x -= mod;
+    if (x >= m) {
+      x -= m;
     }
     return *this;
   }
   constexpr ModInt &operator-=(const ModInt &y) {
     x -= y.x;
-    if (x >= mod) {
-      x += mod;
+    if (x >= m) {
+      x += m;
     }
     return *this;
   }
   constexpr ModInt &operator*=(const ModInt &y) {
-    x = (ull)x * (ull)y.x % mod;
+    x = (ull)x * (ull)y.x % m;
     return *this;
   }
   constexpr ModInt &operator/=(const ModInt &y) { return *this *= y.inv(); }
   constexpr ModInt operator+() const { return *this; }
-  constexpr ModInt operator-() const { return ModInt(mod - x); }
+  constexpr ModInt operator-() const { return ModInt(m - x); }
 
   constexpr ModInt pow(ll n) const {
     if (n < 0) {
@@ -82,7 +83,7 @@ public:
   }
   constexpr ModInt inv() const {
     assert(x);
-    return pow(mod - 2);
+    return pow(m - 2);
   }
 
   constexpr friend ModInt operator+(const ModInt &a, const ModInt &b) {
@@ -104,14 +105,14 @@ public:
     return a.x != b.x;
   }
 
-  friend istream &operator>>(istream &is, ModInt &m) {
+  friend istream &operator>>(istream &is, ModInt &t) {
     ll y;
     is >> y;
-    m.set(y);
+    t.set(y);
     return is;
   }
-  friend ostream &operator<<(ostream &os, const ModInt &m) {
-    os << m.x;
+  friend ostream &operator<<(ostream &os, const ModInt &t) {
+    os << t.x;
     return os;
   }
 };
